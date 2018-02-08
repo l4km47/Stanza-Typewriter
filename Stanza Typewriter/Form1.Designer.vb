24e09a73c6cf8252e85c4b0e087b83e51e14aaf7
Rashan Hasaranga <codexlanka000@gmail.com>
2018-02-08 08:18:11 +0530

    Add files via upload

---
+++ Stanza Typewriter/Form1.Designer.vb
@@ -0,0 +1,209 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.tmrExplandExplorer = New System.Windows.Forms.Timer(Me.components)
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblCharCount = New System.Windows.Forms.Label()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader3 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader4 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader5 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.tmrStatus = New System.Windows.Forms.Timer(Me.components)
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Location = New System.Drawing.Point(13, 12)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.ScriptErrorsSuppressed = True
+        Me.WebBrowser1.ScrollBarsEnabled = False
+        Me.WebBrowser1.Size = New System.Drawing.Size(824, 372)
+        Me.WebBrowser1.TabIndex = 0
+        Me.WebBrowser1.Url = New System.Uri("file:///C:/Users/RshnX5/Desktop/Writer_%20the%20internet%20typewriter.html", System.UriKind.Absolute)
+        '
+        'ListBox1
+        '
+        Me.ListBox1.BackColor = System.Drawing.Color.Black
+        Me.ListBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.ListBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed
+        Me.ListBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListBox1.ForeColor = System.Drawing.Color.Lime
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 15
+        Me.ListBox1.Location = New System.Drawing.Point(12, 48)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(124, 105)
+        Me.ListBox1.TabIndex = 1
+        '
+        'tmrExplandExplorer
+        '
+        Me.tmrExplandExplorer.Interval = 1
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Black
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.lblCharCount)
+        Me.Panel1.Controls.Add(Me.ListView1)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.ListBox1)
+        Me.Panel1.Location = New System.Drawing.Point(13, 270)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(824, 166)
+        Me.Panel1.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.ForeColor = System.Drawing.Color.Lime
+        Me.Label3.Location = New System.Drawing.Point(9, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(16, 31)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "📰"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label2
+        '
+        Me.Label2.ForeColor = System.Drawing.Color.Lime
+        Me.Label2.Location = New System.Drawing.Point(25, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(16, 31)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "💾"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblCharCount
+        '
+        Me.lblCharCount.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCharCount.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(225, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.lblCharCount.Location = New System.Drawing.Point(607, 9)
+        Me.lblCharCount.Name = "lblCharCount"
+        Me.lblCharCount.Size = New System.Drawing.Size(204, 19)
+        Me.lblCharCount.TabIndex = 4
+        Me.lblCharCount.Text = "| Characters : 0 | Words : 0 |"
+        Me.lblCharCount.TextAlign = System.Drawing.ContentAlignment.TopRight
+        '
+        'ListView1
+        '
+        Me.ListView1.BackColor = System.Drawing.Color.Black
+        Me.ListView1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3, Me.ColumnHeader4, Me.ColumnHeader5})
+        Me.ListView1.ForeColor = System.Drawing.Color.Lime
+        Me.ListView1.FullRowSelect = True
+        Me.ListView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable
+        Me.ListView1.Location = New System.Drawing.Point(134, 32)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.OwnerDraw = True
+        Me.ListView1.Size = New System.Drawing.Size(677, 121)
+        Me.ListView1.TabIndex = 3
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.ListView1.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "Name"
+        Me.ColumnHeader1.Width = 330
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Last modified"
+        Me.ColumnHeader2.Width = 140
+        '
+        'ColumnHeader3
+        '
+        Me.ColumnHeader3.Text = "Words"
+        Me.ColumnHeader3.Width = 100
+        '
+        'ColumnHeader4
+        '
+        Me.ColumnHeader4.Text = ""
+        Me.ColumnHeader4.Width = 40
+        '
+        'ColumnHeader5
+        '
+        Me.ColumnHeader5.Text = ""
+        Me.ColumnHeader5.Width = 40
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Lime
+        Me.Label1.Location = New System.Drawing.Point(9, 31)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(40, 15)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Tags :"
+        '
+        'tmrStatus
+        '
+        Me.tmrStatus.Enabled = True
+        Me.tmrStatus.Interval = 1000
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(849, 431)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Stanza Typewriter : New Paper"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents WebBrowser1 As System.Windows.Forms.WebBrowser
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents tmrExplandExplorer As System.Windows.Forms.Timer
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents ListView1 As System.Windows.Forms.ListView
+    Friend WithEvents ColumnHeader1 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader2 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader3 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader4 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents tmrStatus As System.Windows.Forms.Timer
+    Friend WithEvents lblCharCount As System.Windows.Forms.Label
+    Friend WithEvents ColumnHeader5 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+
+End Class
---
+++ Stanza Typewriter/Form1.vb
@@ -0,0 +1,340 @@
+﻿Imports System.Text.RegularExpressions
+Imports System.Runtime.InteropServices
+Imports System.IO
+
+Public Class Form1
+
+
+    Public SavePath As String = "C:\Stanza Typewriter\Saves\"
+
+    Dim panelBounds As Rectangle
+    Public isExplorer = True
+
+    Private ListViewHwnd As Integer = -1
+
+    Private ListViewButtonList As List(Of Button) = New List(Of Button)()
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Panel1.Top = Me.Size.Height - 200
+        Dim AppName As String = My.Application.Info.AssemblyName
+        Dim Root As String = "HKEY_CURRENT_USER\"
+        Dim Key As String = "Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BROWSER_EMULATION"
+        Dim CurrentSetting As String = CStr(Microsoft.Win32.Registry.CurrentUser.OpenSubKey(Key).GetValue(AppName & ".exe"))
+
+        Microsoft.Win32.Registry.SetValue(Root & Key, AppName & ".exe", 11001)
+        Microsoft.Win32.Registry.SetValue(Root & Key, AppName & ".vshost.exe", 11001)
+
+
+
+
+        'get explorer directories
+        For Each dics As String In My.Computer.FileSystem.GetDirectories(SavePath)
+            ListBox1.Items.Add(dics.Replace(SavePath, ""))
+        Next
+        ListBox1.SelectedIndex = 0
+    End Sub
+
+    Private Sub Form1_MouseMove(sender As Object, e As MouseEventArgs) Handles Me.MouseMove
+        If isExplorer = True Then
+            tmrExplandExplorer.Enabled = True
+        End If
+
+    End Sub
+
+
+
+
+
+    Private Sub Form1_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+        Panel1.Top = Me.Size.Height - 200
+    End Sub
+
+    Private Sub tmrExplandExplorer_Tick(sender As Object, e As EventArgs) Handles tmrExplandExplorer.Tick
+        If isExplorer = True Then
+            Panel1.Location = New Point(Panel1.Location.X, Panel1.Location.Y + 5)
+            If Panel1.Location.Y >= Me.Size.Height - 70 Then
+                isExplorer = False
+                tmrExplandExplorer.Enabled = False
+            End If
+        Else
+            Panel1.Location = New Point(Panel1.Location.X, Panel1.Location.Y - 5)
+
+            If Panel1.Location.Y <= Me.Size.Height - 200 Then
+                isExplorer = True
+                tmrExplandExplorer.Enabled = False
+            End If
+        End If
+    End Sub
+
+
+
+    Private Sub ListBox1_DrawItem(sender As Object, e As DrawItemEventArgs) Handles ListBox1.DrawItem
+        ' e.DrawBackground()
+
+
+
+
+        Dim isItemSelected As Boolean = ((e.State And DrawItemState.Selected) = DrawItemState.Selected)
+        Dim itemIndex As Integer = e.Index
+
+        If itemIndex >= 0 AndAlso itemIndex < ListBox1.Items.Count Then
+            Dim g As Graphics = e.Graphics
+            Dim backgroundColorBrush As SolidBrush = New SolidBrush(If((isItemSelected), Color.Lime, Color.Black))
+
+            g.FillRectangle(backgroundColorBrush, e.Bounds)
+            Dim itemText As String = ListBox1.Items(itemIndex).ToString()
+            Dim itemTextColorBrush As SolidBrush = If((isItemSelected), New SolidBrush(Color.White), New SolidBrush(Color.Lime))
+            g.DrawString(itemText, e.Font, itemTextColorBrush, ListBox1.GetItemRectangle(itemIndex).Location)
+            backgroundColorBrush.Dispose()
+            itemTextColorBrush.Dispose()
+        End If
+
+        e.DrawFocusRectangle()
+    End Sub
+
+    Private Sub ListBox1_MouseMove(sender As Object, e As MouseEventArgs) Handles ListBox1.MouseMove
+        Dim item As Integer
+        item = ListBox1.IndexFromPoint(New Point(e.X, e.Y))
+        ListBox1.SelectedIndex = item
+    End Sub
+
+
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        ListView1.Items.Clear()
+        For Each file As String In My.Computer.FileSystem.GetFiles(SavePath & "/" & ListBox1.SelectedItem)
+
+            'modified date
+
+            Dim t As TimeSpan = DateTime.Now - System.IO.File.GetLastWriteTime(file.ToString())
+            Dim modifieddate As String = "Null"
+            If t.TotalSeconds < 60 Then
+                modifieddate = "Less than a minute ago"
+            ElseIf t.TotalMinutes < 60 Then
+                modifieddate = t.Minutes.ToString() + " minutes ago"
+            ElseIf t.Hours < 24 Then
+                modifieddate = t.Hours.ToString() + " hours ago"
+            ElseIf t.Days < 7 Then
+                modifieddate = t.Days.ToString() + " days ago"
+            Else
+                modifieddate = System.IO.File.GetLastWriteTime(file.ToString())
+            End If
+
+
+
+
+
+
+            ' Reading text from a file
+            Dim text = System.IO.File.ReadAllText(file)
+            Dim words = text.Split(" "c)
+            Dim wordCount = words.Length
+            Dim sentences = text.Split("."c, "!"c, "?"c)
+            Dim sentenceCount = sentences.Length
+
+            Dim str(5) As String
+            Dim itm As ListViewItem
+            str(0) = (file.Replace(SavePath & ListBox1.SelectedItem & "\", "")).Replace(".txt", "")
+            str(1) = modifieddate
+            str(2) = wordCount - 2
+            str(3) = "🔒"
+            str(4) = "✖"
+
+            itm = New ListViewItem(str)
+            ListView1.Items.Insert(0, itm)
+        Next
+
+    End Sub
+
+    Private Sub listView1_DrawColumnHeader(ByVal sender As Object, ByVal e As DrawListViewColumnHeaderEventArgs) Handles ListView1.DrawColumnHeader
+
+        Dim strFormat As New StringFormat()
+
+
+        If e.Header.TextAlign = HorizontalAlignment.Center Then
+            strFormat.Alignment = StringAlignment.Center
+        ElseIf e.Header.TextAlign = HorizontalAlignment.Right Then
+            strFormat.Alignment = StringAlignment.Far
+        End If
+
+        e.DrawBackground()
+        e.Graphics.FillRectangle(Brushes.Black, e.Bounds)
+        Dim headerFont As New Font("Microsoft Sans Serif", 8, FontStyle.Regular)
+
+        e.Graphics.DrawString(e.Header.Text, headerFont, Brushes.Lime, e.Bounds, strFormat)
+
+
+
+
+    End Sub
+
+    Private Sub ListView1_DrawItem(sender As Object, e As DrawListViewItemEventArgs) Handles ListView1.DrawItem
+        ' e.DrawDefault = True
+
+        If e.Item.Selected = True Then
+            e.Graphics.FillRectangle(Brushes.Lime, e.Bounds)
+            e.DrawText(TextFormatFlags.TextBoxControl)
+            e.Item.ForeColor = Color.White
+        Else
+            e.Graphics.FillRectangle(Brushes.Black, e.Bounds)
+            e.DrawText(TextFormatFlags.TextBoxControl)
+            e.Item.ForeColor = Color.Lime
+        End If
+
+
+    End Sub
+    Public Function CountWords(ByVal value As String) As Integer
+        ' Count matches.
+        Dim collection As MatchCollection = Regex.Matches(value, "\S+")
+        Return collection.Count
+    End Function
+    Function RemoveWhitespace(fullString As String) As String
+        Return New String(fullString.Where(Function(x) Not Char.IsWhiteSpace(x)).ToArray())
+    End Function
+    Private Sub tmrStatus_Tick(sender As Object, e As EventArgs) Handles tmrStatus.Tick
+        Dim myElement As HtmlElement
+        myElement = WebBrowser1.Document.GetElementById("editor")
+        Dim text As String = myElement.GetAttribute("value")
+
+        Dim WordCound As String = CountWords(text)
+        Dim CharCount As String = RemoveWhitespace(text).Length
+
+
+        lblCharCount.Text = "| Characters : " & CharCount & " | Words : " & WordCound & " |"
+    End Sub
+
+    Private Sub Panel1_MouseMove(sender As Object, e As MouseEventArgs) Handles Panel1.MouseMove
+        If isExplorer = True Then
+            tmrExplandExplorer.Enabled = True
+        Else
+            tmrExplandExplorer.Enabled = True
+        End If
+    End Sub
+    '  Private previousListViewItem As ListViewItem = Nothing
+
+    Private Sub ListView1_DrawSubItem(sender As Object, e As DrawListViewSubItemEventArgs) Handles ListView1.DrawSubItem
+        If e.Item.Selected = True Then
+            e.Graphics.FillRectangle(Brushes.Lime, e.Bounds)
+            e.DrawText(TextFormatFlags.TextBoxControl)
+            e.SubItem.ForeColor = Color.White
+
+        Else
+            e.Graphics.FillRectangle(Brushes.Black, e.Bounds)
+            e.DrawText(TextFormatFlags.TextBoxControl)
+            e.SubItem.ForeColor = Color.Lime
+        End If
+
+    End Sub
+
+
+    Private Sub ListView1_ItemMouseHover(sender As Object, e As ListViewItemMouseHoverEventArgs) Handles ListView1.ItemMouseHover
+        'If previousListViewItem IsNot Nothing Then
+        '    previousListViewItem.ForeColor = Color.White
+        '    previousListViewItem.BackColor = Color.Lime
+        'End If
+
+        e.Item.ForeColor = Color.FromKnownColor(KnownColor.White)
+        e.Item.BackColor = Color.FromKnownColor(KnownColor.Lime)
+
+        'previousListViewItem = e.Item
+
+
+    End Sub
+
+    Private Sub ListView1_MouseUp(sender As Object, e As MouseEventArgs) Handles ListView1.MouseUp
+        Try
+            Dim hti As ListViewHitTestInfo = ListView1.HitTest(e.Location)
+            Dim subitemindex As Integer = hti.Item.SubItems.IndexOf(hti.SubItem)
+            Dim val As String = hti.Item.SubItems(subitemindex).Text
+            If Not subitemindex = Nothing Then
+                If subitemindex.ToString = "4" Then 'delete
+                    If ListBox1.SelectedItem.ToString = "Trash" Then 'trash can
+                        Try
+                            My.Computer.FileSystem.DeleteFile(SavePath & "Trash\" & ListView1.SelectedItems(0).Text & ".txt")
+                        Catch ex As Exception
+                            MsgBox(ex.Message)
+                        End Try
+                    Else
+                        Try
+                            '    My.Computer.FileSystem.DeleteFile()
+                            My.Computer.FileSystem.MoveFile(SavePath & ListBox1.SelectedItem.ToString & "\" & ListView1.SelectedItems(0).Text & ".txt", SavePath & "Trash\" & ListView1.SelectedItems(0).Text & "@" & ListBox1.SelectedItem.ToString & ".txt")
+                        Catch ex As Exception
+                            MsgBox(ex.Message)
+                        End Try
+                    End If
+                End If
+            Else
+                If subitemindex.ToString = "4" Then 'delete
+                    If ListBox1.SelectedItem.ToString = "Trash" Then 'trash can
+                        Try
+                            My.Computer.FileSystem.DeleteFile(SavePath & "Trash\" & ListView1.SelectedItems(0).Text & ".txt")
+                        Catch ex As Exception
+                            MsgBox(ex.Message)
+                        End Try
+                    Else
+                        Try
+                            '    My.Computer.FileSystem.DeleteFile()
+                            My.Computer.FileSystem.MoveFile(SavePath & ListBox1.SelectedItem.ToString & "\" & ListView1.SelectedItems(0).Text & ".txt", SavePath & "Trash\" & ListView1.SelectedItems(0).Text & "@" & ListBox1.SelectedItem.ToString & ".txt")
+                        Catch ex As Exception
+                            MsgBox(ex.Message)
+                        End Try
+                    End If
+                End If
+            End If
+
+        Catch ex As Exception
+
+        End Try
+        Dim x = ListBox1.SelectedIndex
+        If Not x = 0 Then
+            ListBox1.SelectedIndex = 0
+        Else
+            ListBox1.SelectedIndex = 1
+        End If
+
+        ListBox1.SelectedIndex = x
+
+    End Sub
+
+
+    Private Sub ListView1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListView1.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+        Dim myElement As HtmlElement
+        myElement = WebBrowser1.Document.GetElementById("editor")
+        Dim text As String = myElement.GetAttribute("value")
+        If Not text = "" Then
+            If MsgBox("Do you need to save?", MsgBoxStyle.YesNoCancel) = MsgBoxResult.Yes Then
+
+
+                Dim strxFile As String
+                If text.Length > 25 Then
+                    strxFile = SavePath & ListBox1.SelectedItem.ToString & "\" & text.Substring(0, 25).Replace("~", "").Replace("#", "").Replace("%", "").Replace("&", "").Replace("*", "").Replace("{", "").Replace("}", "").Replace("\", "").Replace(":", "").Replace("<", "").Replace(">", "").Replace("?", "").Replace("/", "").Replace("+", "").Replace("|", "").Replace("""", "") & ".txt"
+                Else
+                    strxFile = SavePath & ListBox1.SelectedItem.ToString & "\" & text.Replace("~", "").Replace("#", "").Replace("%", "").Replace("&", "").Replace("*", "").Replace("{", "").Replace("}", "").Replace("\", "").Replace(":", "").Replace("<", "").Replace(">", "").Replace("?", "").Replace("/", "").Replace("+", "").Replace("|", "").Replace("""", "") & ".txt"
+                End If
+
+
+                Dim fileExists As Boolean = File.Exists(strxFile)
+                Using sw As New StreamWriter(File.Open(strxFile, FileMode.OpenOrCreate))
+                    sw.WriteLine( _
+                        IIf(fileExists, _
+                            "Error Message in  Occured at-- " & DateTime.Now, _
+                            "Start Error Log for today"))
+                End Using
+
+
+
+            End If
+
+        End If
+    End Sub
+End Class
+
+
